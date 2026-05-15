48fce84e2baf008fa90c6dfc64475f16f92b0bb2
Simone Silvestri <silvestri.simone0@gmail.com>
2026-05-15 09:50:49 +0200

    update

--- test/test_oceananigans.jl
+++ test/test_oceananigans.jl
@@ -3,6 +3,7 @@ include("setup_runtests.jl")
 using Oceananigans
 using Oceananigans.Fields: AbstractField
 using SparseArrays
+using LinearAlgebra
 
 function x_node_array(x::AbstractVector, Nx, Ny)
     return Array(repeat(view(x, 1:Nx), 1, Ny))'
@@ -117,5 +118,50 @@ end
         regridder(strided_ll, strided_tg)
 
         @test all(dense_ll .== strided_ll)
+    end
+end
+
+gaussian_bump(λ, φ; λ₀=0, φ₀=0, width=10) = exp(-((λ - λ₀)^2 + (φ - φ₀)^2) / 2width^2)
+
+@testset "XESMF extension" begin
+    @info "Testing XESMF regridding..."
+
+    arch = CPU()
+    z = (-1, 0)
+    southernmost_latitude = -80
+    radius = Oceananigans.defaults.planet_radius
+
+    llg_coarse = LatitudeLongitudeGrid(arch; z, radius,
+                                       size = (176, 88, 1),
+                                       longitude = (0, 360),
+                                       latitude = (southernmost_latitude, 90))
+
+    llg_fine = LatitudeLongitudeGrid(arch; z, radius,
+                                     size = (360, 170, 1),
+                                     longitude = (0, 360),
+                                     latitude = (southernmost_latitude, 90))
+
+    tg = TripolarGrid(arch; size=(360, 170, 1), z, southernmost_latitude, radius)
+
+    for (src_grid, dst_grid) in ((llg_coarse, llg_fine),
+                                 (llg_fine, llg_coarse),
+                                 (tg, llg_fine))
+
+        @info "  Regridding from $(nameof(typeof(src_grid))) to $(nameof(typeof(dst_grid)))"
+
+        src_field = CenterField(src_grid)
+        dst_field = CenterField(dst_grid)
+
+        width = 12         # degrees
+        set!(src_field, (λ, φ, z) -> gaussian_bump(λ, φ; λ₀=150, φ₀=30, width) - 2gaussian_bump(λ, φ; λ₀=270, φ₀=-20, width))
+
+        regridder = XESMF.Regridder(dst_field, src_field)
+        @test regridder.weights isa SparseMatrixCSC
+
+        regrid!(dst_field, regridder, src_field)
+
+        # ∫ dst_field dA ≈ ∫ src_field dA
+        @test isapprox(first(Field(Integral(dst_field, dims=(1, 2)))),
+                       first(Field(Integral(src_field, dims=(1, 2)))), rtol=1e-4)
     end
 end
